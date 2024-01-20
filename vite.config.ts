d35573f35181e14b54eebd63b435e3acf8fa22de
vikrantaroraa <arora.veshu@gmail.com>
2024-01-21 01:30:45 +0530

    feat: added the required configuration for creating library

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { resolve } from "path";
+import dts from "vite-plugin-dts";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), dts({ include: ["lib"] })],
+  build: {
+    copyPublicDir: false, // this will make sure that "Public" directory contents are not included while building the library
+    lib: {
+      entry: resolve(__dirname, "lib/main.ts"),
+      formats: ["es"],
+    },
+  },
   resolve: {
     alias: {
       src: "/src",
